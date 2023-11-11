ac943e14b18e029e0f0aa5f4a795edd839ca9970
Tarek Debich <105755292+TarDeb@users.noreply.github.com>
2023-11-11 18:34:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+from ui import IntegratedApp
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = IntegratedApp()
+    window.show()
+    sys.exit(app.exec_())
 
