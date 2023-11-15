38a0a2867846c6e810675c36c3f537ba379a6ecb
Tarek Debich <105755292+TarDeb@users.noreply.github.com>
2023-11-15 19:14:21 +0100

    Delete ui.py

--- ui.py
+++
@@ -1,9 +0,0 @@
-from PyQt5.QtWidgets import QMainWindow, QLabel, QPushButton, QVBoxLayout, QHBoxLayout, QWidget, QFileDialog, QProgressBar
-from PyQt5.QtGui import QPixmap, QImage
-from PyQt5.QtCore import QUrl
-from PyQt5.QtWebEngineWidgets import QWebEngineView
-import glob
-import os
-import cv2
-from image_processor import correct_distortion, get_gps_coordinates, process_images
-
